1d273f079619cf267566dda8906b33b588b64ee5
Mushthaque Muhammed KPP <95971446+Mushthaquekpp@users.noreply.github.com>
2023-07-05 16:10:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// function(){
+    var a=0
+    var b=0
+    var c=0
+    var e=new Date()
+
+ setInterval( function(){
+    e=new Date();
+    a=e.getSeconds() * 6
+   b=e.getMinutes() * 6
+   c=e.getHours() * 30 + Math.round(b/12)
+   document.getElementById("secondhand").style.transform="rotate("+a+"deg)"
+   document.getElementById("minutehand").style.transform="rotate("+b+"deg)"
+   document.getElementById("hourhand").style.transform="rotate("+c+"deg)"
+  
+
+ },1000
+
+ )
+ 
+
+//  }
